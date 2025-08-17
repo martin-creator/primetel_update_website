a59ac57cc9191625b086da642e940fdfacd5ee7f
martin-creator <martinlubowa@outlook.com>
2025-08-18 01:12:01 +0300

    Updated hoabout us page

--- primetel_website/settings.py
+++ primetel_website/settings.py
@@ -130,7 +130,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'epicare/static']
+STATICFILES_DIRS = [BASE_DIR / 'core/static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 

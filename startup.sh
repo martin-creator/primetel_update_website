f5bb713ba4a8ef4e703b84ce28fd7bfb25a2155a
JoakBouy <b.joak@alustudent.com>
2026-02-20 20:03:50 +0300

    setup render runtime

--- startup.sh
+++ startup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
-echo "Running Django migrations..."
-python manage.py migrate --noinput
+set -o errexit
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-echo "Starting Django server..."
-python manage.py runserver 0.0.0.0:8000
+echo "Running Django migrations..."
+python manage.py migrate --noinput
+
+echo "Starting Gunicorn..."
+exec gunicorn primetel_website.wsgi:application --bind 0.0.0.0:${PORT:-8000}
